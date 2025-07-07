d6ee6f74d2969547652d5842b6226e6b26352d82
oldcat <wwwcat@126.com>
2025-07-07 13:07:41 +0800

    Update diy1.sh

--- RAX3000Me/diy1.sh
+++ RAX3000Me/diy1.sh
@@ -20,7 +20,7 @@ git clone --depth 1 -b main https://github.com/QiYueYiya/openwrt-packages packag
 # Mosdns
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/package-mosdns
+#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/package-mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # Easytier
 #git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/package-easytier
